5d80d7fc81a36cbcf0094f36054be6cab172988d
Michael <michael.ahli99@gmail.com>
2025-03-05 02:42:06 +0700

    enable absolute path
    absolute path was unknown

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   experimental: {
     esmExternals: 'loose'
   },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
